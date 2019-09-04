0703c02aeaf5311cb16429f5ede7593c338141f6
Andre Coelho <andre.ribeiro@iv2.com.br>
2019-09-04 12:47:51 -0300

    mock axios and coverage

--- src/__tests__/store/reducers/techs.test.js
+++ src/__tests__/store/reducers/techs.test.js
@@ -2,10 +2,14 @@ import reducer, { INITIAL_STATE } from '~/store/modules/techs/reducer';
 import * as Techs from '~/store/modules/techs/actions';
 
 describe('Techs reducer', () => {
+  it('DEFAULT', () => {
+    const state = reducer(undefined, {});
+    
+    expect(state).toStrictEqual(INITIAL_STATE);
+  })
+
   it('ADD_TECH', () => {
     const state = reducer(INITIAL_STATE, Techs.addTech('Node.js'));
-
-    console.log('state', state)
 
     expect(state).toStrictEqual(['Node.js']);
   })
--- src/__tests__/store/sagas/techs.test.js
+++ src/__tests__/store/sagas/techs.test.js
@@ -1,15 +1,38 @@
 import { runSaga } from 'redux-saga';
+import MockAdapter from 'axios-mock-adapter';
 
-import { getTechsSuccess } from '~/store/modules/techs/actions'
+import api from '~/services/api';
+
+import { getTechsSuccess, getTechsFailure } from '~/store/modules/techs/actions'
 import { getTechs } from '~/store/modules/techs/sagas';
+
+// criar o mock do axios
+const apiMock = new MockAdapter(api)
 
 describe('Techs saga', () => {
   it('should be able to fetch techs', async () => {
     const dispatch = jest.fn();
 
+    // passa a rora, o status e o valor a ser retornado
+    // simula uma api
+    apiMock.onGet('techs').reply(200, ['Node.js'])
+
     await runSaga({ dispatch }, getTechs).toPromise();
 
     // deve ser chamado com a ação e o valor [Node.js]
     expect(dispatch).toHaveBeenCalledWith(getTechsSuccess(['Node.js']))
-  })
+  });
+
+  it('should fail when api returns error', async () => {
+    const dispatch = jest.fn();
+
+    // passa a rora, o status e o valor a ser retornado
+    // simula uma api
+    apiMock.onGet('techs').reply(500)
+
+    await runSaga({ dispatch }, getTechs).toPromise();
+
+    // deve ser chamado com a ação e o valor [Node.js]
+    expect(dispatch).toHaveBeenCalledWith(getTechsFailure());
+  });
 })
